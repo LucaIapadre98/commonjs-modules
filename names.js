3c2f172c0c0b540eb4cd677cfcd57c2d94ee5712
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-05-08 15:37:52 +0200

    creato il primo file con names

---
+++ names.js
@@ -0,0 +1,22 @@
+const firstName = "Luca";
+const lastName = "Iapadre";
+const names =  namePerson(firstName, lastName);
+
+function namePerson(firstName, lastName){
+    return firstName + "" + lastName;
+}
+
+
+module.exports = {names};
+
+
+
+
+
+
+
+
+
+
+
+
