6cfefffc7487aaa61be4b9b7017c1cc593608b25
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-05-08 16:07:45 +0200

    aggiunta la funzione finale

--- people.js
+++ people.js
@@ -1,5 +1,15 @@
 const {names} = require("./names");
 const{hobbys} = require("./hobbies");
 
-console.log(names);
-console.log(hobbys);
+//console.log(names);
+//console.log(hobbys);
+
+function final () {
+    const namesHobbys = {
+        fullName: names,
+        hobbies: hobbys
+    }
+    console.log(namesHobbys);
+}
+
+final();
